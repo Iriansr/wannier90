e07316733613cd2eb4d658cbb814d32dc51e3a67
Iruko <irukoa@live.com>
2021-11-03 12:13:52 +0100

    Added sub utility_get_degen in module utility.F90. All my changes can be found by searching !ALVARO.

--- src/utility.F90
+++ src/utility.F90
@@ -50,8 +50,65 @@ module w90_utility
   public :: utility_wgauss
   public :: utility_zdotu
   public :: utility_diagonalize
+  public :: utility_get_degen!ALVARO
 
 contains
+
+  !=============================================================!
+  subroutine utility_get_degen(eig,dim,degen_thr,deg)!ALVARO
+
+    !Auxiliary routine to get the degree of degeneracy for a given list.
+    !The list is supposed to have it's elements stored in ascending order:
+    !eig(i+1)>=eig(i). The degeneracy threshold is given by degen_thr.
+    !The results are set up such that if deg(i) = N,
+    !then eig(i) = eig(i+1) = ... = eig(i+N-1).
+    !If the value is nondegenerate or if i<j<i+N-1, then deg(j) = 0.
+
+    use w90_constants, only: dp
+
+    integer, intent(in) :: dim
+    real(kind=dp), intent(in) :: degen_thr
+    real(kind=dp), dimension(dim), intent(in) :: eig
+    integer, dimension(dim), intent(out) :: deg
+
+    integer :: i,j
+
+    deg = 0 
+  
+    do i=1, dim
+        do j=i, dim !In ascending order,
+            if (abs(eig(j)-eig(i)) .LE. degen_thr) then
+                !count number of elements equal to eig(i).
+                deg(i) = deg(i) + 1
+            endif
+        enddo
+    enddo
+
+    !Set to 0 elements appearing only once.
+    do i=1, dim
+        if (deg(i).EQ.1) then
+            deg(i) = 0
+        endif
+    enddo
+
+    !In the case of eig(j+2)-eig(j+1)<degen_thr and eig(j+1)-eig(j)<degen_thr,
+    !but eig(j+2)-eig(j)>degen_thr (closely packed levels),
+    do i=dim-1,2,-1
+        if ((deg(i).GT.0).AND.(deg(i-1).NE.0)) then
+            deg(i-1) = deg(i) + 1   !increase the degeneracy value of the 
+                                    !degenerate level according to the degenerate levels following it.
+            deg(i+1) = 0            !Set the next levels to 0.
+        endif
+    enddo
+    
+    do i=dim-1,1,-1
+        !At this point, the second index of a closely packed level shall be corrected.
+        if ((deg(i) .GE. deg(i+1)).AND.(deg(i+1) .NE. 0)) then
+            deg(i+1) = 0
+        endif
+    enddo
+
+  end subroutine utility_get_degen
 
   !=============================================================!
   subroutine utility_zgemm(c, a, transa, b, transb, n)
